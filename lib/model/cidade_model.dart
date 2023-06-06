0b6177a9a71c9f17534b10c0a9d14132900f3632
Jaqueline-jpg <jaquelineribeeiroo@gmail.com>
2023-06-06 20:48:52 -0300

    Commit1

---
+++ lib/model/cidade_model.dart
@@ -0,0 +1,11 @@
+class Cidade{
+  static const campoCodigo = 'codigo';
+  static const campoNome = 'nome';
+  static const campoUf = 'uf';
+
+  int? codigo;
+  String nome;
+  String uf;
+
+  Cidade({this.})
+}
